e0f1e6c7df421c60e56cdfa64391bc9860ced9a3
Gameboy <Gameboy@DESKTOP-M5JQ5GR>
2025-11-28 12:58:25 +0400

    Methods out of class;

--- String/Main.cpp
+++ String/Main.cpp
@@ -9,106 +9,130 @@ using std::endl;
 #define tab "\t"
 #define delimiter "\n----------------------------------------------------------------------\n"
 
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////			Class declaration (Объявление класса)									//////
+
 class String
 {
 	int size;	//Размер строки в Байтах
 	char* str;	//Указатель на строку в динамической памяти
 public:
-	int get_size()const
-	{
-		return size;
-	}
-	const char* get_str()const
-	{
-		return str;
-	}
-	char* get_str()
-	{
-		return str;
-	}
+	int get_size()const;
+	const char* get_str()const;
+	char* get_str();
 	//				Costructors:
-	explicit String(int size = 80) :size(size), str(new char[size] {})
-	{
-		//this->size = size;
-		//this->str = new char[size] {};
-		cout << "DefConstructor:\t" << this << endl;
-	}
-	String(const char str[]) :size(strlen(str) + 1), str(new char[size] {})
-	{
-		//this->size = strlen(str) + 1;
-		//this->str = new char[size] {};
-		for (int i = 0; str[i]; i++)this->str[i] = str[i];
-		cout << "Constructor:\t" << this << endl;
-	}
-	String(const String& other) :size(other.size), str(new char[size] {})
-	{
-		//this->size = other.size;
-		//this->str = new char[size] {};
-		for (int i = 0; i < size; i++)this->str[i] = other.str[i];
-		cout << "CopyConstructor:\t" << this << endl;
-	}
-	String(String&& other)
-	{
-		//String&& - r-value reference
-		//Shallow copy - Поверхностное копирование:
-		this->size = other.size;
-		this->str = other.str;
-		//Обязательно нужно обнулить копируемый объект:
-		other.size = 0;
-		other.str = nullptr;	//nullptr - это указатель на '0' (Указатель в никуда).
-								//nullptr - это физический '0' (Нулевая ячейка памяти).
-		//Это предотвращает удаление динамической памяти деструктором.
-		cout << "MoveConstructor:" << this << endl;
-	}
-	~String()
-	{
-		delete[] str;
-		cout << "Desctructor:\t" << this << endl;
-	}
+	explicit String(int size = 80);
+	String(const char str[]);
+	String(const String& other);
+	String(String&& other);
+	~String();
 
 	//			  Operators:
-	String& operator=(const String& other)
-	{
-		if (this == &other)return *this;
-		delete[] this->str;
-		//Deep copy:
-		this->size = other.size;
-		this->str = new char[size] {};
-		for (int i = 0; i < size; i++)this->str[i] = other.str[i];
-		cout << "CopyAssigment:\t" << this << endl;
-		return *this;
-	}
-	String& operator=(String&& other)
-	{
-		if (this == &other)return *this;
-		delete[] this->str;
+	String& operator=(const String& other);
+	String& operator=(String&& other);
 
-		this->size = other.size;
-		this->str = other.str;
-
-		other.size = 0;
-		other.str = nullptr;
-
-		cout << "MoveAssignment:\t" << endl;
-		return *this;
-	}
-
-	char operator[](int i)const
-	{
-		return str[i];
-	}
-	char& operator[](int i)
-	{
-		return str[i];
-	}
+	char operator[](int i)const;
+	char& operator[](int i);
 
 	//				Methods:
-	void print()const
-	{
-		cout << "Size:\t" << size << endl;
-		cout << "Str:\t" << str << endl;
-	}
+	void print()const;
 };
+
+int String::get_size()const
+{
+	return size;
+}
+const char* String::get_str()const
+{
+	return str;
+}
+char* String::get_str()
+{
+	return str;
+}
+//				Costructors:
+String::String(int size) :size(size), str(new char[size] {})
+{
+	//this->size = size;
+	//this->str = new char[size] {};
+	cout << "DefConstructor:\t" << this << endl;
+}
+String::String(const char str[]) :String(strlen(str) + 1)		/*size(strlen(str) + 1), str(new char[size] {})*/
+{
+	for (int i = 0; str[i]; i++)this->str[i] = str[i];
+	cout << "Constructor:\t" << this << endl;
+}
+String::String(const String& other) :String(other.str)
+{
+	cout << "CopyConstructor:\t" << this << endl;
+}
+String::String(String&& other)
+{
+	//String&& - r-value reference
+	//Shallow copy - Поверхностное копирование:
+	this->size = other.size;
+	this->str = other.str;
+	//Обязательно нужно обнулить копируемый объект:
+	other.size = 0;
+	other.str = nullptr;	//nullptr - это указатель на '0' (Указатель в никуда).
+	//nullptr - это физический '0' (Нулевая ячейка памяти).
+//Это предотвращает удаление динамической памяти деструктором.
+	cout << "MoveConstructor:" << this << endl;
+}
+String::~String()
+{
+	delete[] str;
+	cout << "Desctructor:\t" << this << endl;
+}
+
+//			  Operators:
+String& String::operator=(const String& other)
+{
+	if (this == &other)return *this;
+	delete[] this->str;
+	//Deep copy:
+	this->size = other.size;
+	this->str = new char[size] {};
+	for (int i = 0; i < size; i++)this->str[i] = other.str[i];
+	cout << "CopyAssigment:\t" << this << endl;
+	return *this;
+}
+String& String::operator=(String&& other)
+{
+	if (this == &other)return *this;
+	delete[] this->str;
+
+	this->size = other.size;
+	this->str = other.str;
+
+	other.size = 0;
+	other.str = nullptr;
+
+	cout << "MoveAssignment:\t" << endl;
+	return *this;
+}
+
+char String::operator[](int i)const
+{
+	return str[i];
+}
+char& String::operator[](int i)
+{
+	return str[i];
+}
+
+////////////////			Class declaration end (Конец Объявление класса)									//////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////			Class defenition (Определение класса)									//////
+
+//				Methods:
+void String::print()const
+{
+	cout << "Size:\t" << size << endl;
+	cout << "Str:\t" << str << endl;
+}
 
 String operator+(const String& left, const String& right)
 {
@@ -129,9 +153,12 @@ std::ostream& operator<<(std::ostream& os, const String& obj)
 	return os << obj.get_str();
 }
 
+////////////////			Class deferetion end (Конец Определение класса)									//////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 //#define BASE_CHECK
-//#define OPERATORS_CHECK
-#define CALLING_CONSTRUCTORS
+#define OPERATORS_CHECK
+//#define CALLING_CONSTRUCTORS
 
 void main()
 {
@@ -191,6 +218,7 @@ void main()
 	String str9 = str3;	   //CopyConstructor
 	String str10(str9);	   //CopyConstructor
 	String str11{ str9 };  //CopyConstructor
+	str11.print();
 
 	String str12 = str3 + str7; //MoveConstructor
 	str12.print();
